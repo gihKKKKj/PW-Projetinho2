78c191468bfcadaeea36bc1b0b38f6e520820d05
gihKKKKj <gigimcdp@gmail.com>
2025-06-11 21:15:01 -0300

    Corrigido import com letras maiúsculas

--- src/Pages/Projetos/index.jsx
+++ src/Pages/Projetos/index.jsx
@@ -1,6 +1,6 @@
-import Header from '../../Components/Header'
-import Footer from '../../Components/Footer'
-import Container from '../../Components/container'
+import Header from '../../components/header'
+import Footer from '../../components/footer'
+import Container from '../../components/container'
 
 
 function Projetos() {
